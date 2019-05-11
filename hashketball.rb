7dca99eda575b8109d9f11a4a3635cab6a2fa9b3
Nicole Dow <nikki.dow71@gmail.com>
2019-05-11 13:26:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -156,11 +156,10 @@ end
 
 def player_stats(name)
   player_stats = nil 
-  
-  game_hash.each do |home_away, team_data_set|
-    team_data_set.each do |attribute, data|
-      if attribute == :players
-        attribute.each do |player_name, stats|
+  game_hash.each do |home_away, team_info|
+    team_info.each do |data_label, data|
+      if data_label == :players
+        data.each do |player_name, stats|
           if player_name == name
             player_stats = stats
           end
