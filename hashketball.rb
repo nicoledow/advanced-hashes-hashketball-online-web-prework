45867f0493ae66a09cd0d16e90d6d76eb05ab5be
Nicole Dow <nikki.dow71@gmail.com>
2019-05-11 13:07:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,6 +133,7 @@ def player_numbers(team)
   
   if hash[:home][:team_name] == team
     hash[:home][:players].each do |player_name, info|
+      binding.pry
       jersey_numbers << player_name[:number]
     end
   end
