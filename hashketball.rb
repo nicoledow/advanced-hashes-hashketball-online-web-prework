57d4d77d975b096da746f42bf1ad3a26095432d8
Nicole Dow <nikki.dow71@gmail.com>
2019-05-11 12:51:42 +0000

    Automatically backed up by Learn

---
+++ hashketball.rb
@@ -0,0 +1,146 @@
+
+require "pry"
+
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        "Reggie Evans" => {
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+        },
+        "Brook Lopez" => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
+        },
+        "Mason Plumlee" => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5
+        },
+        "Jason Terry" => {
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+        }
+      },
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        "Jeff Adrien" => {:number => 4, :shoe =>18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7,:slam_dunks => 2
+        },
+        "Bismack Biyombo" => {:number => 0,:shoe => 16, :points => 12,:rebounds => 4, :assists => 7, :steals => 7, :blocks => 15,:slam_dunks => 10},
+        "DeSagna Diop" => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+        },
+        "Ben Gordon" => {:number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
+        "Brendan Haywood" => {:number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 22, :blocks => 5, :slam_dunks => 12}
+      },
+    }
+  }
+end
+
+def num_points_scored(player)
+  hash = game_hash
+  hash.each do |location, info| 
+    info.each do |attribute, stats|
+      if stats.include?(player) 
+       return hash[location][attribute][player][:points]
+      end
+    end
+  end
+end
+
+def shoe_size(player)
+  hash = game_hash
+  
+  hash.each do |location, info|
+    info.each do |attribute, data_set|
+      if data_set.include?(player)
+        return hash[location][attribute][player][:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team)
+  hash = game_hash
+  
+  hash.each do |location, team_data_set|
+    if team_data_set[:team_name] == team
+      return hash[location][:colors]
+    end
+  end
+end
+
+def team_names
+  names_of_teams = []
+  hash = game_hash
+  
+  hash.each do |location, team_data_set|
+    names_of_teams << hash[location][:team_name]
+  end
+  
+  names_of_teams
+end
+
+def player_numbers(team)
+  jersey_numbers = []
+  hash = game_hash
+  
+  hash.each do |location, team_data_set|
+    team_data_set.each do |attribute, info|
+      if info.include?(team)
+        attribute.each do |stat, data|
+          binding.pry
+        end
+      end
+    end
+  end
+  
+end
+
+player_numbers("Brooklyn Nets")
